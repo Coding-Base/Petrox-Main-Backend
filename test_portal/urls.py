8ef16b67a8a21e3227a52726827f38208a15bbf5
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-02-20 14:40:47 +0100

    Add files via upload

--- test_portal/urls.py
+++ test_portal/urls.py
@@ -1,6 +1,8 @@
 # test_portal/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from exams.staff_admin import staff_admin_site
+from exams.views import StaffRegisterView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,6 +10,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('staff-admin/', staff_admin_site.urls),
+    path('api/staff/signup/', StaffRegisterView.as_view(), name='staff-signup'),
+    path('staff/signup/', StaffRegisterView.as_view(), name='staff-signup'),
     path('api/', include('exams.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
